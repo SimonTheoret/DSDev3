20d034e8dbbb939335e31aeb57f433a9dc8a44c0
Simon Theoret <simonteoret@hotmail.com>
2023-10-07 19:06:32 -0400

    Prog done. Analysis started

--- chess_rating.py
+++ chess_rating.py
@@ -64,8 +64,8 @@ def parse_xml(path: str) -> pd.DataFrame:
     for player in root:
         player_data = {}
         for target in TARGET_ATTRIBUTES:
-            value = _get_val(entry = player, tag = target)
-            player_data[target]= value
+            value = _get_val(entry=player, tag=target)
+            player_data[target] = value
         players_list.append(player_data)
 
     print(players_list[0])
@@ -100,7 +100,7 @@ def clean_data(df: pd.DataFrame, year_cutoff: int) -> pd.DataFrame:
     df["rating"] = df["rating"].apply(lambda rating: int(rating))
     # TODO: Gardez juste les joueurs avec une date de naissance jusqu'à
     # year_cutoff (inclusivement)
-    df = df[df['birthday'] <= year_cutoff ]
+    df = df[df["birthday"] <= year_cutoff]
     return df
 
 
@@ -140,12 +140,18 @@ def bin_counts(df: pd.DataFrame, bins: list, bin_centers: list) -> pd.DataFrame:
         )
 
     # TODO: Astuce - utilisez pd.cut, et assurez-vous d'utiliser reset_index() quand utile
-    hist = None
+    hist = pd.DataFrame(
+        pd.cut(df["rating"], bins=bins, include_lowest=True, labels=bin_centers)
+    )
+    hist = pd.DataFrame(hist["rating"].value_counts()).reset_index()
 
     # TODO: Renommer les colonnes, assurez qu'ils ont encore du sens (nous voulons 'rating', 'count')
-    # Note: Vous ne devriez pas avoir à renommer les colonnes. Si vous devez, vous avez peut-être une version vielle de Pandas ce qui pourrait amener à des problèmes avec le autograder.
+    # Note: Vous ne devriez pas avoir à renommer les colonnes. Si vous devez,
+    # vous avez peut-être une version vielle de Pandas ce qui pourrait amener à
+    # des problèmes avec le autograder.
 
     # TODO: Ajoutez la colonne 'count_norm'
+    hist["count_norm"] = hist["count"] / len(df)
 
     return hist
 
@@ -155,11 +161,16 @@ class PermutationTest:
         """Implémente l'expérience de test de permutation.
 
         Arguments :
-            df (pd.DataFrame) : trame de données complète à partitionner (c'est-à-dire inclut les deux groupes)
-            n_overrep (entier) : nombre d'éléments dans le groupe surreprésenté
-            n_underrep (entier) : nombre d'éléments dans le groupe sous-représenté
+            df (pd.DataFrame) : trame de données complète à partitionner
+            (c'est-à-dire inclut les deux groupes)
 
-        n_overrep + n_underrep devrait être == len(df) ! Techniquement < len(df) est correct aussi...
+            n_overrep (entier) : nombre d'éléments dans le groupe surreprésenté
+
+            n_underrep (entier) : nombre d'éléments dans le groupe
+            sous-représenté
+
+        n_overrep + n_underrep devrait être == len(df) ! Techniquement < len(df)
+        est correct aussi...
         """
         if len(df) < n_overrep + n_underrep:
             raise ValueError(f"Sum of n_overrep + n_underrep must be <= len(df)")
@@ -169,55 +180,74 @@ class PermutationTest:
 
     # TODO - complétez cette méthode
     def job(self, seed: int = None) -> Tuple[int, int]:
-        """Échantillonne deux groupes de taille n_overrep, n_underrep et renvoie la note maximale pour chacun des
-        groupe dans cet ordre (overrep, underrep)
+        """Échantillonne deux groupes de taille n_overrep, n_underrep et renvoie
+        la note maximale pour chacun des groupe dans cet ordre (overrep,
+        underrep)
 
         Arguments :
             seed (int, optionnel) : définit l'état aléatoire, si spécifié.
 
         Retour:
-            Tuple[int, int] : les notes maximales pour chacun des deux groupes, dans l'ordre (max(surreprésentation), max(sous-représentation))
+            Tuple[int, int] : les notes maximales pour chacun des deux groupes,
+            dans l'ordre (max(surreprésentation), max(sous-représentation))
         """
         if seed is not None:
             np.random.seed(seed)
 
-        # TODO : échantillonnez deux groupes de taille n_overrep, n_underrep et renvoyez la note maximale pour chacun
-        # des groupe dans l'ordre (overrep, underrep)
+        # TODO : échantillonnez deux groupes de taille n_overrep, n_underrep et
+        # renvoyez la note maximale pour chacun des groupe dans l'ordre
+        # (overrep, underrep)
+        ratings = self.df["rating"].to_numpy()
+        ratings = np.random.permutation(ratings)
+        big_max = max(ratings[0 : self.n_overrep])
+        small_max = max(ratings[self.n_overrep : self.n_overrep + self.n_underrep])
 
-        # Astuce : pensez à utiliser np.random.permutation sur un choix approprié de tableau
-        return
+        # Astuce : pensez à utiliser np.random.permutation sur un choix
+        # approprié de tableau
+        return big_max, small_max
 
 
 # TODO - complétez cette méthode
 def sample_two_groups(
     df: pd.DataFrame, n_overrep: int, n_underrep: int, n_iter: int = 1000
 ) -> Tuple[np.array, np.array]:
-    """Exécutez des tests de permutation n_iter sur df, divisé en deux groupes (échantillonnés SANS remplacement) de
-    taille n_overrep et n_underrep. Renvoyez deux tableaux numpy de longueur n_iter, ou les éléments correspondent à la
-    note maximale dans les groupes surreprésentés et sous-représentés respectivement.
+    """Exécutez des tests de permutation n_iter sur df, divisé en deux groupes
+    (échantillonnés SANS remplacement) de taille n_overrep et n_underrep.
+    Renvoyez deux tableaux numpy de longueur n_iter, ou les éléments
+    correspondent à la note maximale dans les groupes surreprésentés et
+    sous-représentés respectivement.
 
     Arguments :
         df (pd.DataFrame) : dataframe nettoyé à traiter
+
         n_overrep (entier) : nombre d'échantillons pour le groupe surreprésenté
-        n_underrep (entier) : nombre d'échantillons pour le groupe sous-représenté
+
+        n_underrep (entier) : nombre d'échantillons pour le groupe
+        sous-représenté
+
         n_iter (entier, facultatif) : le nombre total d'itérations à exécuter
 
     Retour:
-        Tuple[np.array, np.array] : Deux tableaux contenant les maximums pour le groupe surreprésenté et le groupe sous-représenté pour chacune des expériences n_iter.
+        Tuple[np.array, np.array] : Deux tableaux contenant les maximums pour le
+        groupe surreprésenté et le groupe sous-représenté pour chacune des
+        expériences n_iter.
     """
     best_over = []
     best_under = []
+    permu = PermutationTest(df, n_overrep, n_underrep)
+    for i in tqdm(range(n_iter)):
+        big_max, small_max = permu.job()
+        best_over.append(big_max)
+        best_under.append(small_max)
 
-    # TODO : exécutez n_iter runs de cette expérience et renvoyez un tableau numpy contenant les valeurs maximales des groupes surreprésentés et sous-représentés respectivement.
 
-    # Astuce : enveloppez votre itérateur avec tqdm pour obtenir une barre de progression, par exemple :
+    # TODO : exécutez n_iter runs de cette expérience et renvoyez un tableau
+    # numpy contenant les valeurs maximales des groupes surreprésentés et
+    # sous-représentés respectivement.
+
+    # Astuce : enveloppez votre itérateur avec tqdm pour obtenir une barre de
+    # progression, par exemple :
     # >>> for i in tqdm(range(10)):
     # >>>     print(i)
 
     return np.array(best_over), np.array(best_under)
-
-# def main():
-#     parse_xml("./data/standard_oct22frl_xml.zip")
-
-# if __name__ == "__main__":
-#     main()
